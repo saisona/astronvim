6f0c5ec67b43db6c50e449b7a3217e7b945a0752
Alexandre Saison <alexandre.saison@inarix.com>
2023-04-11 19:25:32 +0200

    Update: adding new vim plugings

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,6 +1,21 @@
+-- TODO: TEST !
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    event = "User Astrofile",
+    cmd = { "TodoQuickFix" },
+    keys = {
+      {
+        "<leader>T",
+        "<cmd>TodoTelescope<cr>",
+        desc = "Open Todo in Telescope",
+      },
+    },
+  },
   -- "andweeb/presence.nvim",
   -- {
   --   "ray-x/lsp_signature.nvim",
