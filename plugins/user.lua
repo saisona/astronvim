706b7337b15232e4f2f4ac223f248a796a553da0
Alexandre Saison <alexandre.saison@inarix.com>
2023-05-17 17:27:44 +0200

    Adding new new config

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,3 @@
--- TODO: TEST !
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
@@ -13,6 +12,31 @@ return {
         "<leader>T",
         "<cmd>TodoTelescope<cr>",
         desc = "Open Todo in Telescope",
+      },
+    },
+  },
+  {
+    "klen/nvim-test",
+    event = "User Astrofile",
+    opts = {},
+    keys = {
+      {
+        "<leader>U",
+        "<cmd>TestNearest<cr>",
+        desc = "Run nearest test",
+      },
+    },
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    event = "User Astrofile",
+    run = function() vim.fn["mkdp#util#install"]() end,
+    config = function() require("markdown-preview.nvim").setup() end,
+    opts = {},
+    keys = {
+      {
+        "<leader>P",
+        "<cmd>MarkdownPreview<cr>",
       },
     },
   },
