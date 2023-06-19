6de6b21660dbfd62f6bd19c3928b7f15d75956d2
Alexandre Saison <alexandre.saison@inarix.com>
2023-06-19 16:59:46 +0200

    Update: adding marks and some ensure_installed for golang

--- plugins/user.lua
+++ plugins/user.lua
@@ -33,6 +33,25 @@ return {
     version = "*",
     config = true,
   },
+  {
+    "chentoast/marks.nvim",
+    event = "User Astrofile",
+    opts = {
+      default_mappings = true,
+      builtin_marks = { ".", "<", ">", "^" },
+      cyclic = true,
+      force_write_shada = false,
+      refresh_interval = 250,
+      sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
+      excluded_filetypes = {},
+      bookmark_0 = {
+        sign = "⚑",
+        virt_text = "hello world",
+        annotate = false,
+      },
+      mappings = {},
+    },
+  },
   -- "andweeb/presence.nvim",
   -- {
   --   "ray-x/lsp_signature.nvim",
